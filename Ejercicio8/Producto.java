edbb01b2a20db4c6d757c7e147a504cfe3714de1
25680160-design <25680160@cuautla.tecnm.mx>
2025-12-05 22:01:04 -0600

    Create Producto.java

---
+++ Ejercicio8/Producto.java
@@ -0,0 +1,47 @@
+public class Producto {
+
+    private String nombre;
+    private double precioOriginal;
+    private double porcentajeDescuento;
+
+
+    public Producto(String nombre, double precioOriginal, double porcentajeDescuento) {
+        this.nombre = nombre;
+        this.precioOriginal = precioOriginal;
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecioOriginal() {
+        return precioOriginal;
+    }
+
+    public void setPrecioOriginal(double precioOriginal) {
+        this.precioOriginal = precioOriginal;
+    }
+
+    public double getPorcentajeDescuento() {
+        return porcentajeDescuento;
+    }
+
+    public void setPorcentajeDescuento(double porcentajeDescuento) {
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
+
+
+    public double calcularMontoDescuento() {
+        return precioOriginal * (porcentajeDescuento / 100);
+    }
+
+    public double calcularPrecioFinal() {
+        return precioOriginal - calcularMontoDescuento();
+    }
+}
